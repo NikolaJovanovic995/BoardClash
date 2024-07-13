d46b783ad0b2c57b543b1a1fa52cdc73406a9447
Nikola Jovanovic <nikola.jovanovic@playstudios-il.com>
2024-07-13 21:28:55 +0200

    Barebone chess

--- Board Clash/Assets/Scripts/Input System/UIInputHandler.cs
+++ Board Clash/Assets/Scripts/Input System/UIInputHandler.cs
@@ -1,18 +1,18 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class UIInputHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
